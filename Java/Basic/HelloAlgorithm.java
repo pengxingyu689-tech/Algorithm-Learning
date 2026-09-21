b516f1b55f30ce54c647bfed3922d38610a8899f
pengxingyu689-tech <331859845+pengxingyu689-tech@users.noreply.github.com>
2026-09-21 11:42:00 +0800

    Add basic Java example

---
+++ Java/Basic/HelloAlgorithm.java
@@ -0,0 +1,7 @@
+import java.util.*;
+import java.io.*;
+public class HelloAlgorithm {
+    public static void main(String[] args) {
+        System.out.println("Start learning algorithms!");
+    }
+}
